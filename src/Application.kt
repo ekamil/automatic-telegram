c0cfdc6f1342a4c9a61aecb9fadf2910fb88821f
Kamil Essekkat <kamil@essekkat.pl>
2021-09-22 17:56:04 +0200

    Initial commit

---
+++ src/Application.kt
@@ -0,0 +1,33 @@
+package pl.essekkat
+
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.jackson.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+fun Application.module(
+) {
+
+    install(ContentNegotiation) {
+        jackson(contentType = ContentType.Application.Json) {
+            enable(SerializationFeature.INDENT_OUTPUT)
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            registerModule(JavaTimeModule())
+        }
+    }
+
+    routing {
+        get("/health") {
+            call.respond(HttpStatusCode.OK, mapOf("status" to "healthy"))
+        }
+    }
+}
+
---
+++ test/ApplicationTest.kt
@@ -0,0 +1,22 @@
+package pl.essekkat
+
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ module() }) {
+            handleRequest(HttpMethod.Get, "/health").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals(
+                    """|{
+                    |  "status" : "healthy"
+                    |}""".trimMargin(), response.content
+                )
+            }
+        }
+    }
+}
